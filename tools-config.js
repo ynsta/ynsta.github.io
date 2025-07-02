045526dca2e081bf5a3d2290c01ba76478f0bace
ynsta <1948171+ynsta@users.noreply.github.com>
2025-07-02 15:10:06 +0000

    Automated: Updated tools-config.js

--- tools-config.js
+++ tools-config.js
@@ -1,4 +1,8 @@
 const toolsConfig = [
     {
-     }
+        "folder": "badge-converter",
+        "file": "index.html",
+        "title": "Badge Serial → ID Converter",
+        "description": "Converts NFC badge serials to decimal ID for Mifare Classic 1K and DESFire EV2"
+    }
 ];
